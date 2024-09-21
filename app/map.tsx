09f08d6da4fa740c84a296882bbd5d50c9c68bb8
Daniel Prada <danielprada2012@gmail.com>
2024-09-20 23:47:54 -0400

    Added overlay of ground elevation

--- app/map.tsx
+++ app/map.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker, TrafficLayer, GroundOverlay } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
@@ -36,7 +36,7 @@ function MapComponent() {
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string
   })
 
-  const [, setMap] = React.useState(null)
+  const [,setMap] = React.useState(null)
   const [trafficData, setTrafficData] = React.useState([] as TrafficData[])
 
   React.useEffect(() => {
@@ -55,6 +55,24 @@ function MapComponent() {
     return <></>
   }
 
+  // https://imageserverintra.miamidade.gov/arcgis/rest/services/DEMs/2021_5ft_DEM/ImageServer/exportImage?f=image&bandIds=&renderingRule=%7B%22rasterFunction%22%3A%22Stretched_5ftDEMRender_BlueToBrown_2021%22%7D&bbox=-8996916.243885973%2C2926329.1924757934%2C-8886006.115844334%2C2992523.6589706666&imageSR=102100&bboxSR=102100&size=1451%2C866
+  // const bounds = {
+  //   north: 25.7877,   // Northern latitude (near Omni area)
+  //   south: 25.7651,   // Southern latitude (near Brickell area)
+  //   east: -80.1845,   // Eastern longitude (near Biscayne Bay)
+  //   west: -80.2017,   // Western longitude (near I-95)
+  // };
+
+  // -8996916.243885973%2C2926329.1924757934%2C-8886006.115844334%2C2992523.6589706666
+  const boundEast = -79.79
+  const heightDiff = 1.1
+  const bounds2 = {
+    north: 26.0,   // Northern latitude (near Omni area)
+    south: 25.5,   // Southern latitude (near Brickell area)
+    east: boundEast,   // Eastern longitude (near Biscayne Bay)
+    west: boundEast - heightDiff,   // Western longitude (near I-95)
+  }
+
   return (
     <GoogleMap
       mapContainerStyle={containerStyle}
@@ -63,6 +81,12 @@ function MapComponent() {
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
+      <GroundOverlay
+        url={'https://imageserverintra.miamidade.gov/arcgis/rest/services/DEMs/2021_5ft_DEM/ImageServer/exportImage?f=image&bandIds=&renderingRule=%7B%22rasterFunction%22%3A%22Stretched_5ftDEMRender_BlueToBrown_2021%22%7D&bbox=-8996916.243885973%2C2926329.1924757934%2C-8886006.115844334%2C2992523.6589706666&imageSR=102100&bboxSR=102100&size=1451%2C866'}
+        bounds={bounds2}
+        opacity={0.5}
+      />
+      <TrafficLayer />
       {
         trafficData.map((trafficDataItem, index) => {
           return (
