251abc55282504c31d25dd8edac447fdecf68b9e
Daniel Prada <danielprada2012@gmail.com>
2024-09-20 20:32:24 -0400

    Added basic google map

---
+++ app/map.tsx
@@ -0,0 +1,41 @@
+'use client'
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { useCallback, useState } from "react";
+
+const MapComponent = () => {
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback((map) => {
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(() => {
+    setMap(null);
+  }, []);
+
+  const mapStyles = {
+    height: "400px",
+    width: "100%",
+  };
+
+  const defaultCenter = {
+    lat: 40.73061, // Default center point (NYC coordinates)
+    lng: -73.935242,
+  };
+
+  return (
+    <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={13}
+        center={defaultCenter}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        <Marker position={defaultCenter} />
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default MapComponent;
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import MapComponent from "./map";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-
+        <MapComponent />
         <p>Under Construction for Broward Crabs</p>
       </main>
       <footer className={styles.footer}>
