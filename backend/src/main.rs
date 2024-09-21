c772d25a91a7b500a24e42249621c04b27746252
Christerpher Hunter <djhunter67@gmail.com>
2024-09-20 21:46:19 -0400

    fix: .gitignore conflict

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use backend::{settings, startup::Application, telemetry};
+use std::io;
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    dotenv::dotenv().ok();
+
+    let settings = settings::get().expect("Failed to get application settings");
+
+    let subscriber = telemetry::get_subcriber(settings.clone().debug);
+    telemetry::init_subscriber(subscriber);
+
+    let application = Application::build(settings, None).await?;
+
+    tracing::event!(target: "aj_studying", tracing::Level::INFO, "Listening on port http://127.0.0.1:{}/", application.port());
+
+    application.run_until_stopped().await?;
+
+    Ok(())
 }
