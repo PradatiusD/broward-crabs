4961dce40f62d60d0d3893b07a88f9af4ede0274
allensftort <allenstfort1@gmail.com>
2024-09-20 22:59:14 -0400

    edit look up page adding logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import "./../app/globals.css";
 import React from "react";
+import logo from './logo.svg';
+import Image from "next/image";
+
 function App({ Component, pageProps }) {
   return (
     <>
       <header>
         <div>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand mx-auto" href="#">Flood Flow</a>
+            <a className="navbar-brand mx-auto" href="#"> <Image src={logo} /></a>
           </nav>
         </div>
       </header>
--- pages/lookup.tsx
+++ pages/lookup.tsx
@@ -1,27 +1,26 @@
 import React from "react";
 
-export default function Lookup () {
+export default function LandingPage () {
     return (
-      <>
+        <>
             <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-                <form className="p-4 border rounded bg-light" style={{ width: '300px' }}>
-                    <div className="form-group">
-                        <label htmlFor="Zipcode">ZipCode</label>
+                <form>
+                    <div className="form-group zip-code pt-4">
                         <input type="text" className="form-control" id="Zipcode" aria-describedby="ZipcodeHelp"
                                placeholder="Enter Zipcode"/>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="CarMake">Make</label>
-                        <input type="text" className="form-control" id="CarMake" aria-describedby="CarMakeHelp"
-                               placeholder="Enter Car Make"/>
+                    <div className="form-group pt-4">
+                        <select className="form-control">
+                            <option>Car Make</option>
+                        </select>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="CarMode">Model</label>
-                        <input type="text" className="form-control" id="CarMode" aria-describedby="CarModeHelp"
-                               placeholder="Enter Car Model "/>
+                    <div className="car-model pt-4">
+                        <select className="form-control">
+                            <option>Car Model</option>
+                        </select>
                     </div>
                 </form>
             </div>
-      </>
+    </>
     );
 }
