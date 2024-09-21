15cf5d1d66e31abf3f8e3524cab9730a56039a5b
allensftort <allenstfort1@gmail.com>
2024-09-20 23:54:00 -0400

    modifed the lookup pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import "./../app/globals.css";
 import React from "react";
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import Image from "next/image";
+import instagram from "./instagram.svg";
+import twitter from "./twitter.svg";
 
 function App({ Component, pageProps }) {
   return (
@@ -10,13 +12,22 @@ function App({ Component, pageProps }) {
       <header>
         <div>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand mx-auto" href="#"> <Image src={logo} /></a>
+            <a className="navbar-brand mx-auto" href="#"> <Image src={logo} alt="logo"/></a>
           </nav>
         </div>
       </header>
       <main>
         <Component {...pageProps} />
       </main>
+        <footer className="d-flex p-2 justify-content-center">
+            <div>
+                contactus@floodflow.com
+            </div>
+            <div>
+                <a><Image src={instagram}  alt="instagram logo"/></a>
+                <a><Image src={twitter}  alt="instagram logo"/></a>
+            </div>
+        </footer>
     </>
   );
 }
--- pages/lookup.tsx
+++ pages/lookup.tsx
@@ -3,22 +3,24 @@ import React from "react";
 export default function LandingPage () {
     return (
         <>
-            <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+            <div className="d-flex justify-content-center align-items-center" style={{ minHeight: 'calc(100vh - 102px)' }}>
                 <form>
+                <h5 className="header-label">Where are you headed?</h5>
                     <div className="form-group zip-code pt-4">
                         <input type="text" className="form-control" id="Zipcode" aria-describedby="ZipcodeHelp"
                                placeholder="Enter Zipcode"/>
                     </div>
                     <div className="form-group pt-4">
-                        <select className="form-control">
+                        <select className="form-select" aria-label="Default select example">
                             <option>Car Make</option>
                         </select>
                     </div>
-                    <div className="car-model pt-4">
-                        <select className="form-control">
+                    <div className="car-model pt-4 pb-4">
+                        <select className="form-select" aria-label="Default select example">
                             <option>Car Model</option>
                         </select>
                     </div>
+                    <button type="button" className="btn btn-primary btn-md rounded-lg">Large button</button>
                 </form>
             </div>
     </>
